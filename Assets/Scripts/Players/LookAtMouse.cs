2092179f22c66192f8b811f0c45ce897794dbf78
DavidCT <deltacharlitangp@gmail.com>
2022-01-20 00:15:05 +0100

    MOD Scena killer llums + ADD Main Camera Script

--- Assets/Scripts/Players/LookAtMouse.cs
+++ Assets/Scripts/Players/LookAtMouse.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] 
     Transform target;
     [SerializeField]
     Camera mainCamera;
---
+++ Assets/Scripts/World/MoveCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform Player;
+    public float PosX;
+    public float PosY;
+    public float PosZ = -10;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x - PosX,
+                                         Player.transform.position.y - PosY, PosZ);
+        
+    }
+}
